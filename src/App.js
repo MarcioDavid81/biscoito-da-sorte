67f9ed1a3078d892ff0b6993bec9b9909a92c296
marciodavid <marciodavid81@gmail.com>
2024-08-05 23:10:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from 'react';
+import './estilo.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            textoFrase: ''
+        }
+
+        this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+        this.frases = [
+            'Sigam-me os bons',
+            'São 48 centavos de leite derramados na minha mesa',
+            'Eu conheço o Maicão',
+            'Cara ela tá tão na sua',
+            'Carinha que mora logo ali, passa um dolar',
+            'Eu não preciso disso, meu marido tem dois empregos',
+            'Ela é branca?',
+            'Trágico',
+            'Não fui eu',
+            'Grande homem'
+        ]
+    }
+
+    quebraBiscoito() {
+        let state = this.state;
+        let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+        state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "';
+        this.setState(state);
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <img className='img' src={require('./assets/biscoito.png')} />
+                <Button nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
+                <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+            </div>
+        );
+    }
+}
+
+class Button extends Component {
+    render() {
+        return (
+            <div>
+                <button className='btnAbrir' onClick={this.props.acaoBtn}>{this.props.nome}</button>
+            </div>
+        );
+    }
 }
 
 export default App;
+ 
